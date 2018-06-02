3515227b264b951d0c878b649e1b899c27eefe94
AnonPrimo <esichkaisi@gmail.com>
2018-06-02 19:34:36 +0300

    Добавьте файлы проекта.

---
+++ Foto/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Foto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton1.Checked = true;
+            button1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double cena = 0;
+            int n;
+            double sum;
+
+            if (radioButton1.Checked)
+                cena = 8.50;
+            if (radioButton2.Checked)
+                cena = 10;
+            if (radioButton3.Checked)
+                cena = 15.5;
+
+            n = Convert.ToInt32(textBox1.Text);
+            sum = n * cena;
+
+            label3.Text = "Цена: " + cena.ToString("C") +
+                "\nКоличевство: " + n.ToString() + "шт.\n" +
+                "Сумма заказа: " + sum.ToString("C");
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9')
+                return;
+
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                    button1.Focus();
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+                button1.Enabled = false;
+            else
+                button1.Enabled = true;
+            label3.Text = "";
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "";
+            textBox1.Focus();
+        }
+    }
+}
